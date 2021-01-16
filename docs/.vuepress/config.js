4917211e16dd6dc03236e5d5487b1481d478efb1
tantanke <1552832259@qq.com>
2021-01-16 09:31:11 +0800

    测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "一只小鸭子",
+  description: 'web前端技术博客,简洁至上,专注web前端学习与总结。',
+   base: '/keke_blog/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
